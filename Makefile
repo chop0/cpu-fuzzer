99d431bf341bf89b40f215468fef65d59dab9b2a
Alec Petridis <alec@xz.ax>
2023-11-05 21:23:32 -0800

    clean up

--- Makefile
+++ Makefile
@@ -1,7 +1,6 @@
 ANTLR_JAR = antlr-4.13.1-complete.jar
 MODULE_PATH = $(ANTLR_JAR):iced-x86-1.20.0.jar
 
-.PHONY: all clean run parse bindings ax.xz.fuzz
 run: ax.xz.fuzz libslave.so
 	java --enable-preview -Djava.library.path=. -p $(MODULE_PATH):out -m ax.xz.fuzz/ax.xz.fuzz.Main
 
@@ -10,14 +9,14 @@ all: libslave.so ax.xz.fuzz
 libslave.so: test_entry_routine.S slave.c slave.h
 	gcc -fPIC -shared -o $@ $^
 
-ax.xz.fuzz: $(shell find . -name "*.java") parse bindings
+ax.xz.fuzz: $(shell find ax.xz.fuzz -name "*.java") parse bindings
 	javac --enable-preview --source 21 -p $(MODULE_PATH) -d out --module ax.xz.fuzz --module-source-path .:gen
 
 bindings: slave.h jextract_exports.txt
 	jextract -l slave -t ax.xz.fuzz.tester --source slave.h --output gen/ax.xz.fuzz @jextract_exports.txt
 
 parse: Operand.g4
-	java -jar $(ANTLR_JAR) -Dlanguage=Java -o gen/ax.xz.fuzz/ax/xz/fuzz/parse -package ax.xz.fuzz.parse Operand.g4
+	java -jar $(ANTLR_JAR) -Dlanguage=Java -o gen/ax.xz.fuzz/ax/xz/fuzz/parse -package ax.xz.fuzz.parse $^
 
 clean:
 	rm -rf out/* libslave.so ax.xz.fuzz/ax/xz/fuzz/parse ax.xz.fuzz/ax/xz/fuzz/tester
--- slave.c
+++ slave.c
@@ -16,26 +16,61 @@
 #define CODE_SIZE 4096
 #define CODE_REGION ((void*)0x41414000)
 
-// Forward declarations
-static void setup_signal_handlers(void);
-static void save_signal_handlers(void);
-static void restore_signal_handlers(void);
-static void segfault_handler(int signal, siginfo_t *si, void *unused);
-void save_and_return(void);
-
-
 void jump_to_code(void *code, struct saved_state *saved_state);
 void test_case_finish(void);
-void restore_fs_gs_base(void);
 
 static struct fault_details fault_details = {0};
 static bool faulted = false;
 
 static uint8_t EPILOGUE[] = { 0xFF, 0x25, 0x00, 0x00, 0x00, 0x00 }; // jmpq *0x0
 
+static int ignored_signals[] = { SIGSEGV, SIGBUS, SIGILL, SIGFPE, SIGTRAP };
+#define NUM_IGNORED_SIGNALS (sizeof(ignored_signals) / sizeof(ignored_signals[0]))
+static struct sigaction saved_sigactions[NUM_IGNORED_SIGNALS];
+
+__attribute__ ((no_stack_protector))  // sicne we mess with fs
+static void segfault_handler(int signal, siginfo_t *si, void *ptr) {
+    faulted = true;
+    fault_details = (struct fault_details){si->si_signo, si->si_code, (void*)(si->si_addr - CODE_REGION)};
+    test_case_finish(); // doesnt return
+}
+
+static void setup_signal_handlers(void) {
+    for (int i = 0; i < NUM_IGNORED_SIGNALS; i++) {
+        struct sigaction sa;
+        sa.sa_flags = SA_SIGINFO | SA_NODEFER | SA_ONSTACK | SA_RESETHAND;
+        sigemptyset(&sa.sa_mask);
+        sa.sa_sigaction = segfault_handler;
+        if (sigaction(ignored_signals[i], &sa, NULL) == -1) {
+            perror("sigaction");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+}
+
+static void restore_signal_handlers(void) {
+    for (int i = 0; i < NUM_IGNORED_SIGNALS; i++) {
+        if (sigaction(ignored_signals[i], &saved_sigactions[i], NULL) == -1) {
+            perror("sigaction");
+            exit(EXIT_FAILURE);
+        }
+        saved_sigactions[i] = (struct sigaction){0};
+    }
+}
+
+static void save_signal_handlers(void) {
+    for (int i = 0; i < NUM_IGNORED_SIGNALS; i++) {
+        saved_sigactions[i] = (struct sigaction){0};
+        if (sigaction(ignored_signals[i], NULL, &saved_sigactions[i]) == -1) {
+            perror("sigaction");
+            exit(EXIT_FAILURE);
+        }
+    }
+}
 
 void do_test(uint8_t *code, size_t code_length, struct execution_result *output) {
-static bool initialized = false;
+    static bool initialized = false;
     if (!initialized) {
             void *signal_stack = malloc(SIGSTKSZ);
             if (signal_stack == NULL) {
@@ -78,50 +113,4 @@ static bool initialized = false;
     }
 
     restore_signal_handlers();
-}
-
-static int masked_signals[] = { SIGSEGV, SIGBUS, SIGILL, SIGFPE, SIGTRAP };
-#define NUM_MASKED_SIGNALS (sizeof(masked_signals) / sizeof(masked_signals[0]))
-
-static struct sigaction saved_sigactions[NUM_MASKED_SIGNALS];
-
-static void setup_signal_handlers(void) {
-    for (int i = 0; i < NUM_MASKED_SIGNALS; i++) {
-        struct sigaction sa;
-        sa.sa_flags = SA_SIGINFO | SA_NODEFER | SA_ONSTACK | SA_RESETHAND;
-        sigemptyset(&sa.sa_mask);
-        sa.sa_sigaction = segfault_handler;
-        if (sigaction(masked_signals[i], &sa, NULL) == -1) {
-            perror("sigaction");
-            exit(EXIT_FAILURE);
-        }
-    }
-
-}
-
-static void restore_signal_handlers(void) {
-    for (int i = 0; i < NUM_MASKED_SIGNALS; i++) {
-        if (sigaction(masked_signals[i], &saved_sigactions[i], NULL) == -1) {
-            perror("sigaction");
-            exit(EXIT_FAILURE);
-        }
-        saved_sigactions[i] = (struct sigaction){0};
-    }
-}
-
-static void save_signal_handlers(void) {
-    for (int i = 0; i < NUM_MASKED_SIGNALS; i++) {
-        saved_sigactions[i] = (struct sigaction){0};
-        if (sigaction(masked_signals[i], NULL, &saved_sigactions[i]) == -1) {
-            perror("sigaction");
-            exit(EXIT_FAILURE);
-        }
-    }
-}
-
-__attribute__ ((no_stack_protector))  // sicne we mess with fs
-static void segfault_handler(int signal, siginfo_t *si, void *ptr) {
-    faulted = true;
-    fault_details = (struct fault_details){si->si_signo, si->si_code, (void*)(si->si_addr - CODE_REGION)};
-    test_case_finish(); // doesnt return
 }
