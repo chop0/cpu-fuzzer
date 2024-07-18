569c8e9abead0ea74f16879a4d01692024ea2a27
alec <chop@quenda.net>
2024-07-18 13:38:40 -0400

    cleaned up reproducer

--- reproducer/repro.c
+++ reproducer/repro.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <sys/mman.h>
+#include <assert.h>
 
 #include <slave.h>
 
@@ -24,85 +25,64 @@ uint8_t code_data[] = { 0x4D, 0x31, 0xFF, 0x49, 0x83, 0xFF, 0x64, 0x7D, 0x62, 0x
 #define HEXARGS32(x, offset) HEXARGS8(x, offset), HEXARGS8(x, offset+8), HEXARGS8(x, offset+16), HEXARGS8(x, offset+24)
 #define HEXARGS64(x, offset) HEXARGS32(x, offset), HEXARGS32(x, offset+32)
 
-void init() {
-	char *result;
-	result = mmap((void *)0x110000, 8192, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0);
+static void *mmap_rwx(void *address, char const* name) {
+	void *result = mmap(address, 8192, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS | (address ? MAP_FIXED : 0), -1, 0);
 	if (result == MAP_FAILED) {
-		result = "mmap: scratch1";
-		goto fail;
+		printf("mmap: ");
+		perror("name");
+		exit(EXIT_FAILURE);
 	}
 
-	result = mmap((void *)0x210000, 8192, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0);
-	if (result == MAP_FAILED) {
-		result = "mmap: scratch2";
-		goto fail;
-	}
+	assert(address == NULL || result == address);
 
-	result = mmap(NULL, 8192, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-	if (result == MAP_FAILED) {
-		result = "mmap: trampoline";
-		goto fail;
-	}
-	trampoline = result;
+	return result;
+}
+
+static void init() {
+	mmap_rwx((void *)0x110000, "scratch1");
+	mmap_rwx((void *)0x210000, "scratch2");
+	trampoline = mmap_rwx(NULL, "trampoline");
+	code = mmap_rwx(NULL, "code");
+
 	void *trampoline_finish = ((void*)&test_case_exit - (void*)&routine_begin) + trampoline;
 	memcpy(code_data + sizeof(code_data) - 8, &trampoline_finish, 8);
 
-	result = mmap(NULL, 8192, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-	if (result == MAP_FAILED) {
-		result = "mmap: code";
-		goto fail;
-	}
-	code = result;
-
 	memcpy(trampoline, routine_begin, (void *)&routine_end - (void *)&routine_begin);
 	memcpy(code, code_data, sizeof(code_data));
-
-	return;
-
-fail:
-	perror(result);
-	exit(EXIT_FAILURE);
-}
-
-void print_zmm_buffer(uint8_t *buffer) {
-	for (int i = 0; i < 64; i++) {
-		printf("%02x", buffer[i]);
-	}
-	puts("");
 }
 
 int main() {
 	init();
 
-	for (int r = 0;; r++) {
+	for (int r = 0; r < 100; r++) {
 		struct execution_result result = { 0 };
-		uint8_t initial_zmm19[64] = { 0 };
+		uint8_t initial_zmm19[64] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0 };
 		uint8_t initial_zmm24[64] = { 0 };
 
-		memcpy(&result.state.zmm[19], "\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10", 16);
+		memcpy(&result.state.zmm[19], initial_zmm19, 64);
+		memcpy(&result.state.zmm[24], initial_zmm24, 64);
+
 		do_test(trampoline, code, sizeof(code_data), &result);
 		if (result.faulted)
 			printf("faulted: %d\n", result.faulted);
 
 		printf(
-		"attempt %d results:\n"
-		"\tinitial zmm19: " HEXFMT64 "\n"
-		"\tfinal zmm19:   " HEXFMT64 "\n"
-		"\tinitial zmm24: " HEXFMT64 "\n"
-		"\tfinal zmm24:   " HEXFMT64 "\n",
+			"attempt %d results:\n"
+			"\tinitial zmm19: " HEXFMT64 "\n"
+			"\tfinal zmm19:   " HEXFMT64 "\n"
+			"\tinitial zmm24: " HEXFMT64 "\n"
+			"\tfinal zmm24:   " HEXFMT64 "\n",
 		r, HEXARGS64(initial_zmm19, 0), HEXARGS64(result.state.zmm[19], 0), HEXARGS64(initial_zmm24, 0), HEXARGS64(result.state.zmm[24], 0)
 		);
 
-		bool all_zeros = true;
-		for (int i = 0; i < 64; i++) {
-			all_zeros &= result.state.zmm[24][i] == 0;
-		}
+		uint8_t zeros[64] = { 0 };
+		bool all_zeros = memcmp(result.state.zmm[24], zeros, 64) == 0;
 
 		if (all_zeros) {
 			printf("ATTEMPT SUCCEEDED on iteration %d: zmm24 is all zeros, even though it shouldn't be....\n", r);
-			break;
+			return 0;
 		}
 	}
-
-	return 0;
+	perror("failed to find a successful attempt");
+	return 1;
 }
