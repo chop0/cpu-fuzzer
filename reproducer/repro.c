a54608fa3d75ee4734fe27ee68a9d8ca23ff9100
alec <chop@quenda.net>
2024-07-18 15:34:07 -0400

    Add working version using intrinsics

--- reproducer/repro.c
+++ reproducer/repro.c
@@ -4,8 +4,9 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <immintrin.h>
 
-#define HEXFMT8 "%02x%02x%02x%02x%02x%02x%02x%02x"
+#define HEXFMT8 "%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx"
 #define HEXFMT32  HEXFMT8 HEXFMT8 HEXFMT8 HEXFMT8
 
 #define HEXARGS8(x, offset) (x)[offset], (x)[offset+1], (x)[offset+2], (x)[offset+3], (x)[offset+4], (x)[offset+5], (x)[offset+6], (x)[offset+7]
@@ -15,7 +16,7 @@ static uint8_t big_buffer[8192] = { 0 };
 
 static void *scratch1 = big_buffer, *scratch2 = big_buffer + 4096;
 
-static void reproducer_code(uint8_t const *initial_xmm16, uint8_t const *initial_xmm24, uint8_t *final_xmm16, uint8_t *final_xmm24) {
+static void reproducer_code(uint8_t const *initial_xmm16, uint8_t const *initial_xmm24, uint8_t *final_xmm24) {
 	asm (
 		"vmovups (%0), %%xmm16\n"
 		"vmovups (%1), %%xmm24\n"
@@ -36,47 +37,54 @@ static void reproducer_code(uint8_t const *initial_xmm16, uint8_t const *initial
 	);
 
 	asm (
-		"vmovups %%xmm16, (%0)\n"
-		"vmovups %%xmm24, (%1)\n"
-		: : "r"(final_xmm16), "r"(final_xmm24)
+		"vmovups %%xmm24, (%0)\n"
+		: : "r"(final_xmm24)
 		: "memory"
 	);
 }
 
+static void reproducer_good(uint8_t const *initial_xmm16, uint8_t const *initial_xmm24, uint8_t *final_xmm24) {
+	__m64 zero = { 0 };
+
+	__m128 a = _mm_loadu_ps((float const *)initial_xmm16);
+	__m128 result = _mm_loadl_pi(a, &zero);
+
+	_mm_storeu_ps((float *)final_xmm24, result);
+}
+
 int main() {
-	bool found_all_zeros = false;
-	bool found_non_zero = false;
+	srand(0);
+
+	uint8_t test_input[64];
+	for (int i = 0; i < sizeof(test_input); i++) {
+		test_input[i] = rand() & 0xff;
+	}
 
 	for (int r = 0; r < 100; r++) {
-		uint8_t initial_xmm16[32] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0 };
+		uint8_t initial_xmm16[32] = { 0 };
 		uint8_t initial_xmm24[32] = { 0 };
+		memcpy(initial_xmm16, test_input, 64);
 
-		uint8_t final_xmm16[32];
-		uint8_t final_xmm24[32];
+		uint8_t good_result[32], test_result[32];
 
-		memcpy(&final_xmm16, initial_xmm16, 32);
-		memcpy(&final_xmm24, initial_xmm24, 32);
-
-		reproducer_code(initial_xmm16, initial_xmm24, final_xmm16, final_xmm24);
+		reproducer_good(initial_xmm16, initial_xmm24, good_result);
+		reproducer_code(initial_xmm16, initial_xmm24, test_result);
 
 		printf(
 			"attempt %d results:\n"
 			"\tinitial xmm16: " HEXFMT32 "\n"
-			"\tfinal xmm16:   " HEXFMT32 "\n"
 			"\tinitial xmm24: " HEXFMT32 "\n"
-			"\tfinal xmm24:   " HEXFMT32 "\n",
-		r, HEXARGS32(initial_xmm16, 0), HEXARGS32(final_xmm16, 0), HEXARGS32(initial_xmm24, 0), HEXARGS32(final_xmm24, 0)
+			"\texpected result: " HEXFMT32 "\n"
+			"\tactual result:   " HEXFMT32 "\n",
+		r, HEXARGS32(initial_xmm16, 0),HEXARGS32(initial_xmm24, 0), HEXARGS32(good_result, 0), HEXARGS32(test_result, 0)
 		);
 
-		uint8_t zeros[32] = { 0 };
-		bool all_zeros = memcmp(final_xmm24, zeros, 32) == 0;
-
-		found_all_zeros |= all_zeros;
-		found_non_zero |= !all_zeros;
-
-		if (found_all_zeros && found_non_zero) {
-			printf("SUCCESS: found 1 attempt where xmm24 was all zeros and 1 attempt where it was not\n");
+		bool correct_result = memcmp(test_result, good_result, 32) == 0;
+		if (!correct_result) {
+			printf("attempt %d SUCCESS: found an attempt where xmm24 was NOT what we expected\n", r);
 			return 0;
+		} else {
+			printf("attempt %d FAILURE: xmm24 was what we expected;  trying again.\n", r);
 		}
 	}
 	fprintf(stderr, "ALL ATTEMPTS FAILED\n");
