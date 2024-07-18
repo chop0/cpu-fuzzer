21d88d5dafe35016cb7c0530d08c329c8989aa85
alec <chop@quenda.net>
2024-07-18 14:38:32 -0400

    make reprroducer standalone

--- reproducer/repro.c
+++ reproducer/repro.c
@@ -5,17 +5,7 @@
 #include <stdint.h>
 #include <sys/mman.h>
 #include <assert.h>
-
-#include <slave.h>
-
-void *trampoline;
-void *code;
-
-typedef uint8_t byte;
-
-uint8_t code_data[] = { 0x4D, 0x31, 0xFF, 0x49, 0x83, 0xFF, 0x64, 0x7D, 0x62, 0x49, 0xFF, 0xC7, 0x0F, 0x71, 0xF3, 0xD9, 0x67, 0xC5, 0x19, 0x7D, 0x24, 0x25, 0x10, 0x0A, 0x21, 0x00, 0x67, 0x66, 0x44, 0x0F, 0x5B, 0x14, 0x25, 0x20, 0x05, 0x11, 0x00, 0x48, 0x09, 0x34, 0x25, 0xB0, 0x01, 0x11, 0x00, 0x44, 0x0F, 0xBD, 0xCE, 0x67, 0x44, 0x8D, 0x24, 0x25, 0x00, 0x0E, 0x21, 0x00, 0x67, 0xC5, 0xF8, 0x52, 0x24, 0x25, 0x40, 0x09, 0x21, 0x00, 0x67, 0xC4, 0xE2, 0x59, 0xBB, 0x04, 0x25, 0x24, 0x02, 0x21, 0x00, 0x67, 0x62, 0x61, 0x64, 0x00, 0x12, 0x04, 0x25, 0x00, 0x00, 0x11, 0x00, 0x67, 0x62, 0x71, 0xFD, 0x08, 0x16, 0x04, 0x25, 0x58, 0x05, 0x21, 0x00, 0x71, 0x02, 0xEB, 0x00,
-	0xFF, 0x25, 0x00, 0x00, 0x00, 0x00,
- 	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+#include <stdbool.h>
 
 #define HEXFMT8 "%02x%02x%02x%02x%02x%02x%02x%02x"
 #define HEXFMT32  HEXFMT8 HEXFMT8 HEXFMT8 HEXFMT8
@@ -24,6 +14,33 @@ uint8_t code_data[] = { 0x4D, 0x31, 0xFF, 0x49, 0x83, 0xFF, 0x64, 0x7D, 0x62, 0x
 #define HEXARGS8(x, offset) (x)[offset], (x)[offset+1], (x)[offset+2], (x)[offset+3], (x)[offset+4], (x)[offset+5], (x)[offset+6], (x)[offset+7]
 #define HEXARGS32(x, offset) HEXARGS8(x, offset), HEXARGS8(x, offset+8), HEXARGS8(x, offset+16), HEXARGS8(x, offset+24)
 #define HEXARGS64(x, offset) HEXARGS32(x, offset), HEXARGS32(x, offset+32)
+
+static void reproducer_code(uint8_t const *initial_zmm19, uint8_t const *initial_zmm24, uint8_t *final_zmm19, uint8_t *final_zmm24) {
+	asm (
+		"vmovups (%0), %%zmm19\n"
+		"vmovups (%1), %%zmm24\n"
+		: : "r"(initial_zmm19), "r"(initial_zmm24)
+		: "zmm19", "zmm24"
+	);
+
+	asm (
+		"or %%r11, %%r11\n"
+		"vhsubpd (0x210000), %%xmm12, %%xmm12\n"
+		"cvtps2dq (0x110000), %%xmm10\n"
+		"vrsqrtps (0x110000), %%xmm4\n"
+		"vfmsub231ss (0x110000), %%xmm4, %%xmm0\n"
+		"vmovlps (0x110000), %%xmm19, %%xmm24\n"
+		"vmovhpd (0x110000), %%xmm0, %%xmm8\n"
+		: : : "xmm0", "xmm4", "xmm8", "xmm10", "xmm12", "xmm19", "xmm24"
+	);
+
+	asm (
+		"vmovups %%zmm19, (%0)\n"
+		"vmovups %%zmm24, (%1)\n"
+		: : "r"(final_zmm19), "r"(final_zmm24)
+		: "memory"
+	);
+}
 
 static void *mmap_rwx(void *address, char const* name) {
 	void *result = mmap(address, 8192, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS | (address ? MAP_FIXED : 0), -1, 0);
@@ -41,30 +58,25 @@ static void *mmap_rwx(void *address, char const* name) {
 static void init() {
 	mmap_rwx((void *)0x110000, "scratch1");
 	mmap_rwx((void *)0x210000, "scratch2");
-	trampoline = mmap_rwx(NULL, "trampoline");
-	code = mmap_rwx(NULL, "code");
-
-	void *trampoline_finish = ((void*)&test_case_exit - (void*)&routine_begin) + trampoline;
-	memcpy(code_data + sizeof(code_data) - 8, &trampoline_finish, 8);
-
-	memcpy(trampoline, routine_begin, (void *)&routine_end - (void *)&routine_begin);
-	memcpy(code, code_data, sizeof(code_data));
 }
 
 int main() {
 	init();
 
+	bool found_all_zeros = false;
+	bool found_non_zero = false;
+
 	for (int r = 0; r < 100; r++) {
-		struct execution_result result = { 0 };
 		uint8_t initial_zmm19[64] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0 };
 		uint8_t initial_zmm24[64] = { 0 };
 
-		memcpy(&result.state.zmm[19], initial_zmm19, 64);
-		memcpy(&result.state.zmm[24], initial_zmm24, 64);
+		uint8_t final_zmm19[64];
+		uint8_t final_zmm24[64];
 
-		do_test(trampoline, code, sizeof(code_data), &result);
-		if (result.faulted)
-			printf("faulted: %d\n", result.faulted);
+		memcpy(&final_zmm19, initial_zmm19, 64);
+		memcpy(&final_zmm24, initial_zmm24, 64);
+
+		reproducer_code(initial_zmm19, initial_zmm24, final_zmm19, final_zmm24);
 
 		printf(
 			"attempt %d results:\n"
@@ -72,14 +84,17 @@ int main() {
 			"\tfinal zmm19:   " HEXFMT64 "\n"
 			"\tinitial zmm24: " HEXFMT64 "\n"
 			"\tfinal zmm24:   " HEXFMT64 "\n",
-		r, HEXARGS64(initial_zmm19, 0), HEXARGS64(result.state.zmm[19], 0), HEXARGS64(initial_zmm24, 0), HEXARGS64(result.state.zmm[24], 0)
+		r, HEXARGS64(initial_zmm19, 0), HEXARGS64(final_zmm19, 0), HEXARGS64(initial_zmm24, 0), HEXARGS64(final_zmm24, 0)
 		);
 
 		uint8_t zeros[64] = { 0 };
-		bool all_zeros = memcmp(result.state.zmm[24], zeros, 64) == 0;
+		bool all_zeros = memcmp(final_zmm24, zeros, 64) == 0;
 
-		if (all_zeros) {
-			printf("ATTEMPT SUCCEEDED on iteration %d: zmm24 is all zeros, even though it shouldn't be....\n", r);
+		found_all_zeros |= all_zeros;
+		found_non_zero |= !all_zeros;
+
+		if (found_all_zeros && found_non_zero) {
+			printf("SUCCESS: found 1 attempt where zmm24 was all zeros and 1 attempt where it was not\n");
 			return 0;
 		}
 	}
